68d4afb94b5d9e7174c9df6ae7e13e7b4de59483
= <sonoda_umi@163.com>
2020-05-19 16:54:53 +0800

    增加通过生日判断两人是否同为XX后的函数

--- Datetime/utils.py
+++ Datetime/utils.py
@@ -14,3 +14,30 @@ def get_constellation(date_str):
          u'处女座', u'天秤座', u'天蝎座', u'射手座')
     d = ((1, 20), (2, 19), (3, 21), (4, 21), (5, 21), (6, 22), (7, 23), (8, 23), (9, 23), (10, 23), (11, 23), (12, 23))
     return n[len(list(filter(lambda y: y <= (month, day), d))) % 12]
+
+def get_current_datetime():
+    """
+    获取当前时间字符串
+    :return:
+    """
+    time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return time_str
+
+
+def get_post_age(fst_date, snd_date):
+    """
+    用于通过两人生日判断是否为X0后或X5后
+    :param fst_date: datetime.date类型
+    :param snd_date: datetime.date类型
+    :return:
+    """
+    fst_year = fst_date.year
+    snd_year = snd_date.year
+    if round(fst_year//100) == round(snd_year//100):
+        age_half = round(fst_year//10)*10 + 5
+        if fst_year > age_half and snd_year > age_half:
+            return '都是' + str(age_half)[2:] + '后'
+        else:
+            return '都是' + str(round(fst_year//10)*10)[2:] + '后'
+    else:
+        return None
